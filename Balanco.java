d63788e13fd2bfa760d9a7ba6b66b2312bc729df
IwataBruno <iwatabrunojp@gmail.com>
2021-08-14 03:50:08 +0900

    new file

---
+++ Balanco.java
@@ -0,0 +1,11 @@
+class BalancoTrimestral{
+	public static void main(String[] args){
+		int gastoJaneiro = 15000;
+		int gastoFevereiro = 23000;
+		int gastoMarco = 17000;
+		int gastoTrimestre = gastoJaneiro + gastoFevereiro + gastoMarco;		double mediaMensal = 18.3;
+		System.out.println(gastoTrimestre);	
+		System.out.println("Valor da media mensal = "+mediaMensal);
+
+	}
+}
